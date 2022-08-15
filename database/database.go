3f202cd34fd871ee1857e2323db0853c41446ff5
Jakub <gruchalski.kuba@gmail.com>
2022-08-15 02:46:48 +0200

    Working example on how to use structs from other packages, how to make a method for *sql.DB type using a new struct for better separation of code

--- database/database.go
+++ database/database.go
@@ -6,32 +6,20 @@ import (
     //"github.com/go-sql-driver/mysql"
 )
 
+type Database struct {
+    Db *sql.DB
+}
+
 type Post struct {
-    ID      int64
-    Title   string
-    Body    string
-    Url     string
+    ID int64
+    Title string
+    Body string
+    Url string
 }
 
-//func (db *sql.DB) PostByID(id int64) (Post, error) {
-//    var pst Post
-//    row := db.Query("SELECT * FROM post WHERE id = ?", id)
-//    err := row.Scan(&pst.ID, &pst.Title, &pst.Body, &pst.Url); if err != nil {
-//        if err == sql.ErrNoRows {
-//            return pst, fmt.Errorf("postByID %d: no such post", id)
-//        }
-//        return pst, fmt.Errorf("postByID %d: %v", id, err)
-//    }
-//    return pst, nil
-//}
-
-func Test() string {
-    return "Hello World"
-}
-
-func QueryPostByID(id int64, db *sql.DB) (Post, error) {
+func (db *Database) QueryPostByID(id int64) (Post, error) {
     var pst Post
-    row := db.QueryRow("SELECT * FROM post WHERE id = ?", id)
+    row := db.Db.QueryRow("SELECT * FROM post WHERE id = ?", id)
     err := row.Scan(&pst.ID, &pst.Title, &pst.Body, &pst.Url); if err != nil {
         if err == sql.ErrNoRows {
             return pst, fmt.Errorf("postByID %d: no such post", id)
@@ -39,4 +27,28 @@ func QueryPostByID(id int64, db *sql.DB) (Post, error) {
         return pst, fmt.Errorf("postByID %d: %v", id, err)
     }
     return pst, nil
+}
+
+func (db *Database) Test() string {
+    return "Hello World"
+}
+
+func (db *Database) QueryAllPosts() ([]Post, error) {
+    var posts []Post
+    rows, err := db.Db.Query("SELECT * FROM post;")
+    if err != nil {
+        return nil, fmt.Errorf("%v\n", err)
+    }
+    defer rows.Close()
+    for rows.Next() {
+        var pst Post
+        if err := rows.Scan(&pst.ID, &pst.Title, &pst.Body, &pst.Url); err != nil {
+            return nil, fmt.Errorf("%v\n", err)
+        }
+        posts = append(posts, pst)
+    }
+    if err := rows.Err(); err != nil {
+        return nil, fmt.Errorf("%v\n", err)
+    }
+    return posts, nil
 }
--- main.go
+++ main.go
@@ -14,15 +14,14 @@ import (
 
 var db *sql.DB
 
-type Post struct {
-    ID int64
-    Title string
-    Body string
-    Url string
-}
+//type Post struct {
+//    ID int64
+//    Title string
+//    Body string
+//    Url string
+//}
 
 func main() {
-    fmt.Println(mdb.Test())
     cfg := mysql.Config{
         User: os.Getenv("DBUSER"),
         Passwd: os.Getenv("DBPASS"),
@@ -34,9 +33,14 @@ func main() {
 
     var err error
     db, err = sql.Open("mysql", cfg.FormatDSN())
+    mdbsql := mdb.Database{
+        Db: db,
+    }
     if err != nil {
         log.Fatal(err)
     }
+
+    fmt.Println("mdb: ", mdbsql.Test())
 
     pingErr := db.Ping()
     if pingErr != nil {
@@ -44,108 +48,24 @@ func main() {
     }
     fmt.Println("Connected!")
 
-    posts, err1 := queryPosts(1)
+    post, err1 := mdbsql.QueryPostByID(1)
     if err1 != nil {
-        log.Fatal(err)
+        log.Fatal(err1)
     }
-    fmt.Printf("Posts found: %v\n", posts)
+    fmt.Printf("post found: %v\n", post)
 
-    post, err2 := postByID(2)
+    posts, err2 := mdbsql.QueryAllPosts()
     if err2 != nil {
-        log.Fatal(err)
+        log.Fatal(err2)
     }
-    fmt.Printf("Post found: %v\n", post)
-
-    pstID, err3 := addPost(Post{
-        Title: "Test",
-        Body: "for adding data",
-        Url: "tothedb.com",
-    })
-    if err3 != nil {
-        log.Fatal(err3)
+    fmt.Printf("Query all posts: %v\n", posts)
+    pmap := make(map[int]mdb.Post)
+    for i, val := range posts {
+        fmt.Println(i, val)
+        pmap[i] = val
     }
-    fmt.Printf("ID of added post: %v\n", pstID)
-    
-    newPost, err4 := mdb.QueryPostByID(1, db)
-    if err4 != nil {
-        log.Fatal(err4)
+    fmt.Println(pmap)
+    for key, val := range pmap {
+        fmt.Println(key, val.Title) 
     }
-    fmt.Printf("new post %v\n", newPost)
-
-//    err1 := godotenv.Load("local.env")
-//    if err1 != nil {
-//        log.Fatal("Error: %s", err)
-//    }    
-//    
-//    var id, username, password, secret string
-//    id = os.Getenv("CLIENT_ID")
-//    username = os.Getenv("USERNAME")
-//    password = os.Getenv("PASSWORD")
-//    secret = os.Getenv("CLIENT_SECRET")
-//
-//    credentials := reddit.Credentials{ID: id, Secret: secret, Username: username, Password: password}
-//    client, _ := reddit.NewClient(credentials)
-    
-//    posts, _, _ := client.Subreddit.TopPosts(context.Background(), "golang", &reddit.ListPostOptions{
-//        ListOptions: reddit.ListOptions{
-//            Limit: 5,
-//        },
-//        Time: "all",
-//    })
-//    fmt.Printf("Received %d posts.\n", posts)
-//    posts, _, _, err2 := client.User.Saved(context.Background(), &reddit.ListUserOverviewOptions{
-//        ListOptions: reddit.ListOptions{},
-//        Time: "all",
-//    })
-//    if err2 != nil {
-//        fmt.Println(err)
-//    }
-//   fmt.Println(posts)
-//   for _, val := range posts {
-//        fmt.Println(val.Title)
-//   }
-}
-
-func queryPosts(id int64) ([]Post, error) {
-    var posts []Post
-
-    rows, err := db.Query("SELECT * FROM post WHERE ID = ?", id)
-    if err != nil {
-        return nil, fmt.Errorf("queryPosts %q: %v", id, err)
-    }
-    defer rows.Close()
-
-    for rows.Next() {
-        var pst Post
-        if err := rows.Scan(&pst.ID, &pst.Title, &pst.Body, &pst.Url); err != nil {
-            return nil, fmt.Errorf("queryPosts %q: %v", id, err)
-        }
-        posts = append(posts, pst)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("queryPosts %q: %v", id, err)
-    }
-    return posts, nil   
-}
-
-func postByID(id int64) (Post, error) {
-    var pst Post
-
-    row := db.QueryRow("SELECT * FROM post WHERE id = ?", id)
-    if err := row.Scan(&pst.ID, &pst.Title, &pst.Body, &pst.Url); err != nil {
-        return pst, fmt.Errorf("postByID %d: no such post", id)
-    }
-    return pst, nil
-}
-
-func addPost(pst Post) (int64, error) {
-    result, err := db.Exec("INSERT INTO post (title, body, url) VALUES (?, ?, ?)", pst.Title, pst.Body, pst.Url)
-    if err != nil {
-        return 0, fmt.Errorf("addPost: %v", err)
-    }
-    id, err := result.LastInsertId()
-    if err != nil {
-        return 0, fmt.Errorf("addPost: %v", err)
-    }
-    return id, nil
 }
