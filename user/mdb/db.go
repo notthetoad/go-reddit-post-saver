a5e4af6fb8bb3aa769b0d1448c5fe9d4b43801a2
Jakub <gruchalski.kuba@gmail.com>
2022-08-22 16:13:41 +0200

    Base functionality fully working

--- user/mdb/db.go
+++ user/mdb/db.go
@@ -11,18 +11,7 @@ import (
 
 type Database struct {
     Db *sql.DB
-    //Cached map[int]interface{}
 }
-
-//func (db *Database) Cache(data []*reddit.Post) {
-//    if db.Cached == nil {
-//        db.Cached = make(map[int]interface{})
-//    }
-//    for i, value := range data {
-//        db.Cached[i] = value
-//    } 
-//    fmt.Println("cached")
-//}
 
 func (db *Database) InitDb() {
     var err error
@@ -47,38 +36,6 @@ func (db *Database) InitDb() {
     }
     fmt.Println("Connected!")
 }
-
-
-//func (db *Database) QueryPostByID(id int64) (*reddit.Post, error) {
-//    var pst *reddit.Post
-//    row := db.Db.QueryRow("SELECT * FROM post WHERE id = ?", id)
-//    err := row.Scan(&pst.ID, &pst.Title, &pst.Body, &pst.Url); if err != nil {
-//        if err == sql.ErrNoRows {
-//            return pst, fmt.Errorf("postByID %d: no such post", id)
-//        }
-//        return pst, fmt.Errorf("postByID %d: %v", id, err)
-//    }
-//    return pst, nil
-//}
-//
-//func (db *Database) QueryAllPosts() ([]Post, error) {
-//    var posts []Post
-//    rows, err := db.Db.Query("SELECT * FROM post;")
-//    if err != nil {
-//        return nil, fmt.Errorf("%v\n", err)
-//    }
-//    defer rows.Close()
-//    for rows.Next() {
-//        var pst Post
-//        if err := rows.Scan(&pst.ID, &pst.Title, &pst.Body, &pst.Url); err != nil {
-//            return nil, fmt.Errorf("%v\n", err)
-//        }
-//        posts = append(posts, pst)
-//    }
-//    if err := rows.Err(); err != nil {
-//        return nil, fmt.Errorf("%v\n", err)
-//    }
-//    return posts, nil
 
 func (db *Database) SaveSinglePost(p *reddit.Post) error {
     _, err := db.Db.Exec("INSERT IGNORE INTO reddit_post (post_id, permalink, url, title, selftext, subreddit) VALUES (?, ?, ?, ?, ?, ?);", &p.FullID, &p.Permalink, &p.URL, &p.Title, &p.Body, &p.SubredditName)
